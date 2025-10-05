5ce85b2ea94e5f46a41d55cbf9490c6ca71669fa
abdurazakm <abdulkem5472gmail.com>
2025-10-05 11:24:41 +0300

    basic starting template and static file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -54,6 +54,7 @@ ROOT_URLCONF = 'django_blog.urls'
 
 TEMPLATES = [
     {
+        'DIRS': [BASE_DIR / 'templates'],
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
@@ -115,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
