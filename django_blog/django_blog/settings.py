2bd1cc638879ba67b8287b60a4e4c3fb3f900b7c
abdurazakm <abdulkem5472gmail.com>
2025-10-05 11:47:08 +0300

    set dabase to mysql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogapp_db',      # your database name
+        'USER': 'root',            # your MySQL username
+        'PASSWORD': 'A2A2A2A2a@12',            # your MySQL password (if any)
+        'HOST': 'localhost',       # or '127.0.0.1'
+        'PORT': '3306',            # MySQL default port
     }
 }
 
